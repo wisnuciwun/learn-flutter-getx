8884c0081e778f100edd0c8b0c710327afec22d7
Wisnuadiwardhana <wisnu@tokodistributor.com>
2022-07-28 10:06:59 +0700

    learn part 1 - initial project & basic use getx

---
+++ android/app/src/main/kotlin/com/example/learn_flutter_getx/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.learn_flutter_getx
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,129 @@
+import 'dart:ffi';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:get/route_manager.dart';
+
+void main() {
+  runApp(TryApp());
+}
+
+class TryApp extends StatelessWidget {
+  const TryApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(home: Home()); // use getx here
+  }
+}
+
+class HomeController extends GetxController {
+  var dataNum = 0.obs; // observe by getx
+  increment() => dataNum++;
+  decrement() => dataNum--;
+}
+
+class Home extends StatelessWidget {
+  // const Home({Key? key}) : super(key: key);
+  final homectrl = Get.put(HomeController());
+
+  @override
+  Widget build(BuildContext context) {
+    print("yes, its rendered !"); // only once rendered
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text("Stateless page with getx"), centerTitle: true),
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          // Obx(() => Text(
+          //   "${homectrl.dataNum.value}",
+          //   style: TextStyle(fontSize: 20),
+          // )),
+          Obx(() {
+            print("rendered by obx");
+            return Text(
+              "${homectrl.dataNum.value}",
+              style: TextStyle(fontSize: 20),
+            );
+          }),
+          SizedBox(
+            height: 50,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    homectrl.increment();
+                  },
+                  child: Text("+")),
+              SizedBox(
+                width: 10,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    homectrl.decrement();
+                  },
+                  child: Text("-"))
+            ],
+          )
+        ]),
+      ),
+    );
+  }
+}
+
+
+// class Home extends StatefulWidget {
+//   @override
+//   State<Home> createState() => _HomeState();
+// }
+
+// class _HomeState extends State<Home> {
+//   int data = 0;
+
+//   // const Home({Key? key}) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     // stful makes whole page is re-render
+//     print("yes, it's rerender again bro !");
+//     return Scaffold(
+//       appBar: AppBar(title: const Text("Stateless page without getx"), centerTitle: true),
+//       body: Center(
+//         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+//           Text(
+//             "$data",
+//             style: TextStyle(fontSize: 20),
+//           ),
+//           SizedBox(
+//             height: 50,
+//           ),
+//           Row(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: [
+//               ElevatedButton(
+//                   onPressed: () {
+//                     setState(() {
+//                       data = data + 1;
+//                     });
+//                   },
+//                   child: Text("+")),
+//               SizedBox(
+//                 width: 10,
+//               ),
+//               ElevatedButton(
+//                   onPressed: () {
+//                     setState(() {
+//                       data = data - 1;
+//                     });
+//                   },
+//                   child: Text("-"))
+//             ],
+//           )
+//         ]),
+//       ),
+//     );
+//   }
+// }
